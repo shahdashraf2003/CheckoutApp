fe303e8d0439b894114ae55c05be0b83112caf30
shahdashraf2003 <shahdashraf396@gmail.com>
2024-10-20 22:36:24 +0300

    change duration

--- lib/features/checkout/presentation/views/widgets/payment_method_item.dart
+++ lib/features/checkout/presentation/views/widgets/payment_method_item.dart
@@ -13,7 +13,7 @@ class PaymentMethodItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 100),
       width: 103,
       height: 62,
       decoration: ShapeDecoration(
